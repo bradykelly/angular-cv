e9aff4b43c14e79b5bfa517967edb2ea9b44baf8
= <=>
2023-12-07 01:28:02 +0200

    Generated new `prototype` app to compare fresh app to my older one

--- basic-angular/webpack.config.js
+++ basic-angular/webpack.config.js
@@ -1,7 +1,15 @@
 ﻿module.exports = {
-    resolve: {
-        alias: {
-            path: 'path-browserify'
-        }
-    }
+  resolve: {
+    alias: {
+      path: 'path-browserify'
+    },
+    fallback: {
+      "url": require.resolve("url/")
+    },
+    fallback: { "stream": require.resolve("stream-browserify") },
+    fallback: { "os": require.resolve("os-browserify/browser") },
+    fallback: { "os": false },
+    fallback: {"assert": false},
+    fallback: { "constants": false },
+  }
 };
